379ef0c6e031400997f23a42b8ca199e98f89782
nevertheless0404 <nevertheless.hi@gmail.com>
2024-01-06 21:03:06 +0900

    the rules of native

--- App.js
+++ App.js
@@ -1,35 +1,27 @@
-import { Text, SafeAreaView, StyleSheet } from 'react-native';
-
-// You can import supported modules from npm
-import { Card } from 'react-native-paper';
-
-// or any files within the Snack
-import AssetExample from './components/AssetExample';
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <SafeAreaView style={styles.container}>
-      <Text style={styles.paragraph}>
-        Oh Yeah!!!
-      </Text>
-      <Card>
-        <AssetExample />
-      </Card>
-    </SafeAreaView>
+    <View style={styles.container}>
+      <Text style={styles.text}>Hdeloooooooooo</Text>
+      <StatusBar style="auto" />
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    backgroundColor: '#ecf0f1',
-    padding: 8,
+    backgroundColor: '#fff',
+    alignItems: "center",
+    justifyContent: "center",
   },
-  paragraph: {
-    margin: 24,
-    fontSize: 18,
-    fontWeight: 'bold',
-    textAlign: 'center',
-  },
+  text: {
+    fontSize: 28, 
+    color: "red",
+  }
 });
+
+// https://snack.expo.dev/voM-m-WN4
