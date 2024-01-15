9cbfe5f489307258f68bf0ac27f04e85ca9403b5
nevertheless0404 <nevertheless.hi@gmail.com>
2024-01-15 18:55:14 +0900

    layout system

---
+++ App02.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View } from "react-native";
+
+export default function App() {
+  return (
+    <View>
+      <View style={{ width:100, height:100, backgroundcolor: "tomato" }}></View>
+      <View style={{ width:100, height:100, backgroundcolor: "teal" }}></View>
+      <View style={{ width:100, height:100, backgroundcolor: "oragne" }}></View>
+    </View>
+  );
+}
