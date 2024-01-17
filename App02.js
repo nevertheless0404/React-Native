a3cccf551a0763baf7907f2eda6e47aac8eff85b
nevertheless0404 <nevertheless.hi@gmail.com>
2024-01-17 21:32:53 +0900

    styles

--- App02.js
+++ App02.js
@@ -1,12 +1,51 @@
 import React from "react";
-import { View } from "react-native";
+import { View, Text ,StyleSheet } from "react-native";
 
 export default function App() {
-  return (
-    <View>
-      <View style={{ width:100, height:100, backgroundcolor: "tomato" }}></View>
-      <View style={{ width:100, height:100, backgroundcolor: "teal" }}></View>
-      <View style={{ width:100, height:100, backgroundcolor: "oragne" }}></View>
+  return ( 
+    <View style={styles.con}>
+      <View style={styles.city}>
+        <Text style={styles.cityName}>Seoul</Text>
+      </View>
+      <View style={styles.weather}>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  con: {
+    flex: 1, 
+    backgroundColor: "tomato"
+  },
+  city: {
+    flex: 1.2, 
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  cityName: {
+    fontSize: 68,
+    fontWeight: "500",
+  },
+  weather: {
+    flex: 3, 
+  },
+  day:{
+    flex: 1,
+    alignItems: "center",
+  },
+  temp:{
+    marginTop: 50,
+    fontSize: 178,
+  },
+  description: {
+    marginTop: -30,
+    fontSize: 60,
+  }
+})
+
+//https://snack.expo.dev/@yuyeong/mad-violet-pudding
